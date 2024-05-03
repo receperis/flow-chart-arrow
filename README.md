739cc7a179156e48128b8b106c47d33c39ff7260
receperis <receperis@gmail.com>
2024-05-03 23:28:14 +0200

    version stable

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
-# ArrowBetweenNodes
+# Arrow
 
-flow-chart-arrow is a simple React component designed to draw arrows between two HTML elements, indicating a directional relationship. This component is particularly useful in scenarios where you need to visually represent connections between nodes, such as in flow diagrams or organizational charts.
+`flow-chart-arrow` is a simple React component designed to draw arrows between two HTML elements, indicating a directional relationship. This component is particularly useful in scenarios where you need to visually represent connections between nodes, such as in flow diagrams or organizational charts.
 
 ## Installation
 
@@ -17,14 +17,14 @@ To use flow-chart-arrow in your React application, import it and include it in y
 
 ```jsx
 import React from "react";
-import ArrowBetweenNodes from "flow-chart-arrow";
+import Arrow from "flow-chart-arrow";
 
 const MyComponent = () => {
   return (
     <div>
       <div id="startNode">Start Node</div>
       <div id="endNode">End Node</div>
-      <ArrowBetweenNodes
+      <Arrow
         startNodeId="startNode"
         endNodeId="endNode"
         direction="horizontal"
@@ -41,21 +41,18 @@ export default MyComponent;
 
 - `startNodeId` (string, required): The ID of the HTML element representing the starting point of the arrow.
 - `endNodeId` (string, required): The ID of the HTML element representing the ending point of the arrow.
-- `direction` (string, optional): The direction of the arrow. Can be either "horizontal" or "vertical". Defaults to "horizontal".
+- `direction` (string, required): The direction of the arrow. Can be either "horizontal" or "vertical".
 
 ## Features
 
-- **Dynamic Positioning**: ArrowBetweenNodes adjusts its position dynamically based on window resize and scroll events, ensuring accurate representation even when the page layout changes.
+- **Dynamic Positioning**: Arrow adjusts its position dynamically based on window resize and scroll events, ensuring accurate representation even when the page layout changes.
 - **Marker End**: The arrowhead is automatically added to the end of the arrow, enhancing visual clarity.
 
 ## Notes
 
 - This component assumes that the provided start and end nodes are present in the DOM. Ensure that the IDs provided correspond to valid HTML elements.
-- For optimal performance, avoid excessive re-renders of ArrowBetweenNodes by optimizing the parent component's rendering logic.
+- For optimal performance, avoid excessive re-renders of Arrow by optimizing the parent component's rendering logic.
 
 ## License
 
-ArrowBetweenNodes is licensed under the MIT License. Feel free to use, modify, and distribute it as per your requirements.
-```
-
-You can copy and paste this directly into your `README.md` file.
+Arrow is licensed under the MIT License. Feel free to use, modify, and distribute it as per your requirements.
---
+++ src/Arrow.tsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react';
+
+type ArrowProps = {
+    startNodeId: string;
+    endNodeId: string;
+    direction: string;
+}
+
+const Arrow: React.FC<ArrowProps> = ({
+    startNodeId,
+    endNodeId,
+    direction,
+}) => {
+    const [dimensions, setDimensions] = useState<any>({
+        height: window.innerHeight,
+        width: window.innerWidth,
+    });
+    const [scrollPos, setScrollPos] = useState<any>({
+        scrollX: 0,
+        scrollY: 0,
+    });
+    const [arrowPosition, setArrowPosition] = useState<any>({});
+
+    useEffect(() => {
+        const containerNode = document.querySelector('body');
+
+        const handleResize = () => {
+            setDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth,
+            });
+        };
+
+        const handleScroll = () => {
+            setScrollPos({
+                scrollX: containerNode?.scrollLeft,
+                scrollY: containerNode?.scrollTop,
+            });
+        };
+
+        window.addEventListener('resize', handleResize);
+        containerNode?.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            containerNode?.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        const startNode = document.getElementById(startNodeId);
+        const endNode = document.getElementById(endNodeId);
+
+        const calculateArrowPosition = () => {
+            if (!startNode) throw Error('Start node not provided')
+            if (!endNode) throw Error('End node not provided')
+
+            const startRect = startNode.getBoundingClientRect();
+            const endRect = endNode.getBoundingClientRect();
+
+            let startX;
+            let startY;
+            let endX;
+            let endY;
+            let horizontalArrowRightAngleEndY;
+            let horizontalArrowFinalEndY;
+
+            switch (direction) {
+                case 'horizontal':
+                    startX = startRect.right;
+                    startY = startRect.top + startRect.height / 2;
+
+                    endX = endRect.left;
+                    endY = endRect.top + endRect.height / 2;
+                    break;
+
+                case 'vertical':
+                    startX = startRect.left + startRect.width / 2;
+                    startY = startRect.bottom;
+
+                    endX = endRect.left + endRect.width / 2;
+                    endY = endRect.top;
+                    horizontalArrowFinalEndY = endY;
+
+                    horizontalArrowRightAngleEndY = (endY - startY) / 2;
+                    endY = endY - horizontalArrowRightAngleEndY;
+                    break;
+
+                default:
+                    return null;
+            }
+
+            return { startX, startY, endX, endY, horizontalArrowFinalEndY };
+
+        };
+
+        setArrowPosition(calculateArrowPosition());
+    }, [dimensions, direction, endNodeId, scrollPos, startNodeId]);
+
+    const getSVGPath = (): string => {
+        if (direction === 'horizontal') {
+            return `M ${arrowPosition.startX} ${arrowPosition.startY}
+              L ${arrowPosition.endX} ${arrowPosition.endY}`;
+        }
+        if (direction === 'vertical') {
+            return `M ${arrowPosition.startX} ${arrowPosition.startY}
+              L ${arrowPosition.startX} ${arrowPosition.endY}
+              L ${arrowPosition.endX} ${arrowPosition.endY}
+              L ${arrowPosition.endX} ${arrowPosition.horizontalArrowFinalEndY}`;
+        }
+        return '';
+    };
+
+    if (!arrowPosition.startX) {
+        throw Error('Arrow position could not be calculated')
+    }
+
+    return (
+        <svg
+            style={{
+                position: 'absolute',
+                left: 0,
+                top: 0,
+                pointerEvents: 'none',
+                zIndex: 2,
+            }}
+            width="100%"
+            height="100%"
+        >
+            <path
+                d={getSVGPath()}
+                style={{
+                    fill: 'none',
+                    stroke: '#1E81B9',
+                    strokeWidth: 1.2,
+                    markerEnd: 'url(#arrowHead)',
+                }}
+            />
+            <defs>
+                <marker
+                    id="arrowHead"
+                    markerWidth="9"
+                    markerHeight="10"
+                    refX="9"
+                    refY="5"
+                    orient="auto"
+                >
+                    <path
+                        d="M 4 0 L 9 5 L 4 10"
+                        fill="none"
+                        style={{
+                            stroke: '#1E81B9',
+                            strokeWidth: 1,
+                        }}
+                    />
+                </marker>
+            </defs>
+        </svg>
+    );
+};
+
+export default Arrow
